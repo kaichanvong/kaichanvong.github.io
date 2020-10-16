d879f1514facedb440cfc0fed35297077b60d4c4
kai chan vong <kai@kaivong.com>
2020-10-16 15:36:06 +0100

    add enhancements

---
+++ enhancements.js
@@ -0,0 +1,3 @@
+      var document_title = document.title.replace('their','kaivong.com');
+      document.getElementById('notificat0r').innerHTML = document_title;
+      document.getElementsByTagName('h1')[0].append(' lands in your browser!')
