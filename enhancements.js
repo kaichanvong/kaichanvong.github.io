f4d84572564dbbeeb44e260e0941d4c262154d9d
Kai <kai@kaivong.com>
2020-10-16 15:39:04 +0100

    Update enhancements.js

--- enhancements.js
+++ enhancements.js
@@ -1,3 +1,3 @@
-      var document_title = document.title.replace('their','kaivong.com');
+      var document_title = document.title.replace('their','the kaivong.com');
       document.getElementById('notificat0r').innerHTML = document_title;
       document.getElementsByTagName('h1')[0].append(' lands in your browser!')
