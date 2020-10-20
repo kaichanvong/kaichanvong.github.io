b5a767473eb0a054316f780152e4113ea98bbade
kai chan vong <kai@kaivong.com>
2020-10-20 11:21:27 +0100

    update and include new page

---
+++ ui/enhancements.js
@@ -0,0 +1,4 @@
+// function creativifi_that_page_element(tag_selector, message) { document.getElementsByTagName(tag_selector)[0].append(message); }
+var document_title_text = document.title.replace('their','the');
+(document.getElementById('notificat0r').innerHTML = document_title_text);
+// creativifi_that_page_element('h1', ' does use Google web-tracking in your browser');
