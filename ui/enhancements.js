a19c82d49e61729e68d542aefc757c205a4ad95a
kai chan vong <kai@kaivong.com>
2020-10-22 11:48:20 +0100

    edit javascript

--- ui/enhancements.js
+++ ui/enhancements.js
@@ -1,12 +1,21 @@
-if (document.getElementById('notificat0r') == null) {
-} else {
-	var document_title_text = document.title.replace('their','the');
-	(document.getElementById('notificat0r').innerHTML = document_title_text);
-}
+(function() {
+  var doc_url, document_title_text, element_anchor_name_lookup;
 
-var element_anchor_name_lookup = document.URL.split('#')[1];
-if (element_anchor_name_lookup == undefined) {
-} else if (element_anchor_name_lookup == "previously-kai") {
-	document.getElementsByTagName('body')[0].setAttribute('class','highlight');
-} else {
-}
+  if (document.getElementById('notificat0r') === null) {
+
+  } else {
+    document_title_text = document.title.replace('their', 'the');
+    (document.getElementById('notificat0r').innerHTML = document_title_text);
+  }
+
+  doc_url = document.URL;
+
+  element_anchor_name_lookup = doc_url.split('#')[1];
+
+  if (element_anchor_name_lookup === void 0) {
+
+  } else if (element_anchor_name_lookup === "previously-kai") {
+    document.getElementsByTagName('body')[0].setAttribute('class', 'highlight');
+  }
+
+}).call(this);
