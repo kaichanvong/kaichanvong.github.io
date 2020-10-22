b9bc9ef5c71359a691568c3ef40ec3d348c899f0
kai chan vong <kai@kaivong.com>
2020-10-22 11:59:19 +0100

    edit highlight for social-links enhancements

--- ui/enhancements.js
+++ ui/enhancements.js
@@ -1,21 +1,38 @@
 (function() {
-  var doc_url, document_title_text, element_anchor_name_lookup;
+  var anchor_name_split_i, body_class_split_i, correct_notificat0r_text, enhance_social_links, new_text, old_text;
 
-  if (document.getElementById('notificat0r') === null) {
+  correct_notificat0r_text = function(old_text, new_text) {
+    var document_title_text;
+    if (document.getElementById('notificat0r') === null) {
 
-  } else {
-    document_title_text = document.title.replace('their', 'the');
-    (document.getElementById('notificat0r').innerHTML = document_title_text);
-  }
+    } else {
+      document_title_text = document.title.replace(old_text, new_text);
+      return document.getElementById('notificat0r').innerHTML = document_title_text;
+    }
+  };
 
-  doc_url = document.URL;
+  enhance_social_links = function(anchor_name_split_i, body_class_split_i) {
+    var body_class, doc_url, element_anchor_name_lookup;
+    doc_url = document.URL;
+    element_anchor_name_lookup = doc_url.split('#')[anchor_name_split_i];
+    body_class = doc_url.split('/')[body_class_split_i];
+    if (element_anchor_name_lookup === void 0) {
 
-  element_anchor_name_lookup = doc_url.split('#')[1];
+    } else if ((element_anchor_name_lookup === "previously-kai") && (body_class === 'social-links')) {
+      return document.getElementsByTagName('body')[0].setAttribute('class', 'highlight social-links');
+    }
+  };
 
-  if (element_anchor_name_lookup === void 0) {
+  old_text = 'their';
 
-  } else if (element_anchor_name_lookup === "previously-kai") {
-    document.getElementsByTagName('body')[0].setAttribute('class', 'highlight');
-  }
+  new_text = 'the';
+
+  correct_notificat0r_text(old_text, new_text);
+
+  anchor_name_split_i = 1;
+
+  body_class_split_i = 3;
+
+  enhance_social_links(anchor_name_split_i, body_class_split_i);
 
 }).call(this);
