94a520bfd0331bda6be0c87c8494b86e0dda7975
kaichanvong <kai.vong@gmail.com>
2023-09-04 11:35:29 +0100

    edit out js

--- kaivongcom-ui/enhancements copy.js
+++
@@ -1,5 +0,0 @@
-// console.log('hey.... hi... or https://www.kaivong.com/contact ');
-
-var $doc = window.document,
-    content = $doc.getElementById('main-content');
-    // content.prepend(' <span onclick=""> hi! </span>');
