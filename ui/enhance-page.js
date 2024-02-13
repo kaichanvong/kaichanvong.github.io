0f99df3b5b550b44cabf6774ccc9c2fa9870eb9d
kaichanvong <kai.vong@gmail.com>
2024-02-13 13:53:46 +0000

    edit styles

---
+++ ui/enhance-page.js
@@ -0,0 +1,20 @@
+
+
+document.getElementById('topOfPageContent').setAttribute('class','has-javascript');
+
+kaivong_com_objs = { linkedContent: { views: ['view-icons','view-icons'] }  }
+toggle_views = function(){
+  var new_view = this.getAttribute('data-name');
+  linkedContent.setAttribute('class', new_view);
+  linkedContent.setAttribute('data-view', new_view);
+  return false;            
+};            
+
+var socialMediaChannelsToggleLink1 = document.getElementById('socialMediaChannelsToggleLink1'),
+    socialMediaChannelsToggleLink2 = document.getElementById('socialMediaChannelsToggleLink2'),
+    linkedContent = document.getElementById('socialMediaChannelsCompleteContent');
+    socialMediaChannelsToggleLink1.onclick = toggle_views;
+    socialMediaChannelsToggleLink2.onclick = toggle_views;
+
+
+
