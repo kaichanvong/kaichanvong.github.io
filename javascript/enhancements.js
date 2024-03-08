7170455fe31b1df8d0a93a687699bc2386862dd8
kaichanvong <kai.vong@gmail.com>
2024-03-08 11:18:22 +0000

    edit progressive enhancements (adds music information)

--- javascript/enhancements.js
+++ javascript/enhancements.js
@@ -6,7 +6,7 @@
         socialMediaDiv.setAttribute('data-view', new_view);
         return false;
       };            
-      adjustOnlineAddy = function(newInnerHTML){
+      function adjustOnlineAddy(newInnerHTML){
       document.getElementById('current_online_addy').innerHTML = newInnerHTML.replace(https[1],'').replace('/','');
       };
       var socialMediaChannelsToggleLink1 = document.getElementById('socialMediaChannelsToggleLink1'),
@@ -16,13 +16,13 @@
           socialMediaChannelsToggleLink1.onclick = toggle_views;
           socialMediaChannelsToggleLink2.onclick = toggle_views;
 
-      https = ['http://', 'https://'];
-      link_names = ['kaichanvong.github.io/', 'kaivong.com/', 'kaichanvong.online']
-      dot_com_link = https[0] + link_names[1];
-      online_link = https[1] + link_names[2];
+//      https = ['http://', 'https://'];
+//      link_names = ['kaichanvong.github.io/', 'kaivong.com/', 'kaichanvong.online']
+//      dot_com_link = https[0] + link_names[1];
+//      online_link = https[1] + link_names[2];
 
       var huh = "<li class=\"bi horizontal-placeholder\"><em>❓</em></li>",
-          instagram_huh = "<li class=\"bi bi-instagram\"><a href=\"https://www.instagram.com/kaichanvong\" id=\"instagramLinkContent\" title=\"instagram of kai\" rel=\"me\"><span><strong>instagram</strong></span></a></li>"
+          instagram_huh = "<li class=\"bi bi-instagram\"><a href=\"https://www.instagram.com/kaichanvong\" id=\"instagramLinkContent\" title=\"instagram of kai\" rel=\"me\"><span><strong>instagram</strong></span></a></li>",
           more = "<li class=\"bi horizontal-placeholder\"><em>…</em></li>";
       linkedContent.innerHTML += instagram_huh;
       linkedContent.innerHTML += huh;
@@ -31,9 +31,10 @@
       javascript_enabled_message = document.getElementById('off-page-markedup-lols2');
       javascript_enabled_message.setAttribute('class', 'off-page--HTML-only');
 
-      addHackOneLineStylesheetOnHTML = function(args){
-        var read_page = "<style> #off-page-markedup-lols2, .🐰 { position: absolute; left: -9999px; } </style>",
-            HTML_page = document.getElementsByTagName(args)[0]; 
-        HTML_page.innerHTML += read_page;         
-      };
-      addHackOneLineStylesheetOnHTML("body");
+      function addHackOneLineStylesheetOnHTML(pageElement){
+        read_page = "<style> #off-page-markedup-lols2, .🐰 { position: absolute; left: -9999px; } </style>";
+        pageElement.innerHTML += "<style> #off-page-markedup-lols2, .🐰 { position: absolute; left: -9999px; } </style>";         
+      }
+      
+      var HTML_page_optimizr_selector = document.getElementById('downpage-markedup-lols3');
+      addHackOneLineStylesheetOnHTML(HTML_page_optimizr_selector);
