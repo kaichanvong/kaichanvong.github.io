ec6bdd245aef3092c126430b8880767720bd5d6b
kaichanvong <kai.vong@gmail.com>
2024-03-10 16:02:03 +0000

    edit page

--- javascript/enhancements.js
+++ javascript/enhancements.js
@@ -1,20 +1,23 @@
+      var socialMediaChannelsToggleLink1 = document.getElementById('select-icons-view1'),
+          socialMediaChannelsToggleLink2 = document.getElementById('select-list-view2'),
+          socialMediaDiv = document.getElementById('socialMediaChannelsCompleteContent'),
+          linkedContent = document.getElementById('linkedContent'),
+          socialMediaChannels = { 1: 'list' , 0: 'icons'};
+
       document.getElementById('topOfPageContent').setAttribute('class','has-javascript');
-      kaivong_com_objs = { socialMediaDiv: { views: ['view-icons','view-icons'] }  }
-      toggle_views = function(){
-        var new_view = this.getAttribute('data-name');
-        socialMediaDiv.setAttribute('class', new_view);
-        socialMediaDiv.setAttribute('data-view', new_view);
-        return false;
+      kaivong_com_objs = { socialMediaDiv: { views: ['view-icons','view-list'] }  }
+		
+      function toggle_views(){
+		var index_for_view = document.getElementById('select-list-icons-view').selectedIndex,
+		new_view = kaivong_com_objs['socialMediaDiv']['views'][index_for_view];
+		socialMediaDiv.setAttribute('class', new_view);
+		socialMediaDiv.setAttribute('data-view', new_view);
       };            
       function adjustOnlineAddy(newInnerHTML){
       document.getElementById('current_online_addy').innerHTML = newInnerHTML.replace(https[1],'').replace('/','');
       };
-      var socialMediaChannelsToggleLink1 = document.getElementById('socialMediaChannelsToggleLink1'),
-          socialMediaChannelsToggleLink2 = document.getElementById('socialMediaChannelsToggleLink2'),
-          socialMediaDiv = document.getElementById('socialMediaChannelsCompleteContent');
-          linkedContent = document.getElementById('linkedContent');
-          socialMediaChannelsToggleLink1.onclick = toggle_views;
-          socialMediaChannelsToggleLink2.onclick = toggle_views;
+//           socialMediaChannelsToggleLink1.onclick = toggle_views;
+//           socialMediaChannelsToggleLink2.onclick = toggle_views;
 
 //      https = ['http://', 'https://'];
 //      link_names = ['kaichanvong.github.io/', 'kaivong.com/', 'kaichanvong.online']
@@ -24,10 +27,9 @@
       var huh = "<li class=\"bi horizontal-placeholder\"><em>❓</em></li>",
           instagram_huh = "<li class=\"bi bi-instagram\"><a href=\"https://www.instagram.com/kaichanvong\" id=\"instagramLinkContent\" title=\"instagram of kai\" rel=\"me\"><span><strong>instagram</strong></span></a></li>",
           more = "<li class=\"bi horizontal-placeholder\"><em>…</em></li>";
-      linkedContent.innerHTML += instagram_huh;
-      linkedContent.innerHTML += huh;
-      linkedContent.innerHTML += more;
-
+		  linkedContent.innerHTML += instagram_huh;
+//       linkedContent.innerHTML += huh;
+//       linkedContent.innerHTML += more;
       javascript_enabled_message = document.getElementById('off-page-markedup-lols2');
       javascript_enabled_message.setAttribute('class', 'off-page--HTML-only');
 
