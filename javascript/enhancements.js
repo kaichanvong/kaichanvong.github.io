85d0ee8cfb529a197c4ccba88c23df655b42f82e
Kai <kai.vong@kaivong.com>
2024-03-12 20:32:08 +0000

    edit page

--- javascript/enhancements.js
+++ javascript/enhancements.js
@@ -8,12 +8,25 @@ var socialMediaChannelsToggleLink1 = document.getElementById('select-icons-view1
 	more = "<li class=\"bi horizontal-placeholder\"><em>…</em></li>",
 	HTML_page_optimizr_selector = document.getElementById('downpage-markedup-lols3');
 
+select_list_icons_view = '';
+// select_list_icons_view += '<select id="select-list-icons-view" style="display: none;">';
+select_list_icons_view += '	<option value="select-icons-view">';
+select_list_icons_view += '		<a href="#digitalIdentityContent" id="select-icons-view1" data-name="view-icons">icons</a>';
+select_list_icons_view += '	</option>';
+select_list_icons_view += '	<option value="select-list-view">';
+select_list_icons_view += '		<a href="#digitalIdentityContent" id="select-list-view2" data-name="view-list">list</a>';
+select_list_icons_view += '	</option>';
+// select_list_icons_view += '</select>';
+// document.getElementById('section-for-select-box').innerHTML = select_list_icons_view;
+document.getElementById('select-list-icons-view').innerHTML = select_list_icons_view;
+
 document.getElementById('topOfPageContent').setAttribute('class','has-javascript');
 linkedContent.innerHTML += instagram_huh;
 
 kaivong_com_objs = { socialMediaDiv: { views: ['view-icons','view-list'] }  }
 function toggle_views(){
 	index_for_view = document.getElementById('select-list-icons-view').selectedIndex,
+// 	index_for_view = select_list_icons_view.selectedIndex,
 	new_view = kaivong_com_objs['socialMediaDiv']['views'][index_for_view];
 	socialMediaDiv.setAttribute('class', new_view);
 	socialMediaDiv.setAttribute('data-view', new_view);
@@ -23,6 +36,7 @@ if(localStorage.key('kaivong_com_socialmedia_view')){
 	viewName = localStorage['kaivong_com_socialmedia_view'],
 	viewIndex = kaivong_com_objs['socialMediaDiv']['views'].indexOf(viewName);
 	document.getElementById('select-list-icons-view').selectedIndex = viewIndex;
+// 	select_list_icons_view.selectedIndex = viewIndex;
 	toggle_views();
 }
 
@@ -30,8 +44,12 @@ javascript_enabled_message = document.getElementById('off-page-markedup-lols2');
 javascript_enabled_message.setAttribute('class', 'off-page--HTML-only');
 	
 function addHackOneLineStylesheetOnHTML(pageElement){
-	read_page = "<style> #off-page-markedup-lols2, .🐰 { position: absolute; left: -9999px; } </style>";
-	pageElement.innerHTML += "<style> #off-page-markedup-lols2, .🐰 { position: absolute; left: -9999px; } </style>";         
+	read_page = "<style> #off-page-markedup-lols2, #off-page-markedup-lols3, .🐰 { position: absolute; left: -9999px; } </style>";
+	pageElement.innerHTML += read_page
 }
 addHackOneLineStylesheetOnHTML(HTML_page_optimizr_selector);
+
+
+
 document.getElementById('select-list-icons-view').onchange = toggle_views;
+// select_list_icons_view.onchange = toggle_views;
