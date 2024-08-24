660f26ece4f50a8a87758fdea074ba7b6085a4af
Kai Vong <kaivong@hey.com>
2024-08-24 10:53:04 +0100

    edit enhancements.js
    remove, edit variables unused for enhancements

--- javascript/enhancements.js
+++ javascript/enhancements.js
@@ -1,11 +1,7 @@
-var socialMediaChannelsToggleLink1 = document.getElementById('select-icons-view1'),
-	socialMediaChannelsToggleLink2 = document.getElementById('select-list-view2'),
-	socialMediaDiv = document.getElementById('socialMediaChannelsCompleteContent'),
+var socialMediaDiv = document.getElementById('socialMediaChannelsCompleteContent'),
 	linkedContent = document.getElementById('linkedContent'),
 	socialMediaChannels = { 1: 'list' , 0: 'icons'},
-	huh = "<li class=\"bi horizontal-placeholder\"><em>❓</em></li>",
-	instagram_huh = "<li class=\"bi bi-instagram\"><a href=\"https://www.instagram.com/kaichanvong\" id=\"instagramLinkContent\" title=\"instagram of kai\" rel=\"me\"><span><strong>instagram</strong></span></a></li>",
-	more = "<li class=\"bi horizontal-placeholder\"><em>…</em></li>",
+	instagram_list_element = "<li class=\"bi bi-instagram\"><a href=\"https://www.instagram.com/kaichanvong\" id=\"instagramLinkContent\" title=\"instagram of kai\" rel=\"me\"><span><strong>instagram</strong></span></a></li>",
 	HTML_page_optimizr_selector = document.getElementById('downpage-markedup-lols3');
 
 select_list_icons_view = '';
@@ -18,7 +14,7 @@ select_list_icons_view += '	</option>';
 document.getElementById('select-list-icons-view').innerHTML = select_list_icons_view;
 
 document.getElementById('topOfPageContent').setAttribute('class','has-javascript');
-linkedContent.innerHTML += instagram_huh;
+linkedContent.innerHTML += instagram_list_element;
 
 kaivong_com_objs = { socialMediaDiv: { views: ['view-icons','view-list'] }  }
 function toggle_views(){
