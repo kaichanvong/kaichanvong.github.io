939d546b5d38122c18f33f303189bf2ba7602dc1
@kaichanvong <kai@kaivong.com>
2024-12-19 00:46:42 +0000

    edit home page (grid edit)

--- javascript/enhancements.js
+++ javascript/enhancements.js
@@ -6,7 +6,6 @@ var socialMediaChannelsToggleLink1 = document.getElementById('select-icons-view1
 	huh = "<li class=\"bi horizontal-placeholder\"><em>❓</em></li>",
 	instagram_huh = "<li class=\"bi bi-instagram\"><a href=\"https://www.instagram.com/kaivong_com\" id=\"instagramLinkContent\" title=\"instagram of kai\" rel=\"me\"><span><strong>instagram</strong></span></a></li>",
 	more = "<li class=\"bi horizontal-placeholder\"><em>…</em></li>",
-	HTML_page_optimizr_selector = document.getElementById('downpage-markedup-lols3');
 
 select_list_icons_view = '';
 select_list_icons_view += '	<option value="select-icons-view">';
@@ -15,7 +14,6 @@ select_list_icons_view += '	</option>';
 select_list_icons_view += '	<option value="select-list-view">';
 select_list_icons_view += '		<a href="#digitalIdentityContent" id="select-list-view2" data-name="view-list">list</a>';
 select_list_icons_view += '	</option>';
-// document.getElementById('select-list-icons-view').style.display = 'block';
 document.getElementById('select-list-icons-view').innerHTML = select_list_icons_view;
 
 document.getElementById('HTMLPageContent').setAttribute('class','has-javascript');
@@ -23,10 +21,9 @@ linkedContent.innerHTML += instagram_huh;
 
 kaivong_com_objs = { socialMediaDiv: { views: ['view-icons','view-list'] }  }
 function toggle_views(){
-
 	if(this.nodeName == undefined || this.nodeName == 'SELECT' ){
-	index_for_view = document.getElementById('select-list-icons-view').selectedIndex,
-	new_view = kaivong_com_objs['socialMediaDiv']['views'][index_for_view];	
+		index_for_view = document.getElementById('select-list-icons-view').selectedIndex,
+		new_view = kaivong_com_objs['socialMediaDiv']['views'][index_for_view];	
 	}
 	if(this.nodeName == 'A'){
 		$button = this.nodeName;
@@ -48,11 +45,6 @@ if(localStorage.key('kaivong_com_socialmedia_view')){
 javascript_enabled_message = document.getElementById('off-page-markedup-lols2');
 javascript_enabled_message.setAttribute('class', 'off-page--HTML-only');
 	
-function addHackOneLineStylesheetOnHTML(pageElement){
-	read_page = "<style> #off-page-markedup-lols2, #off-page-markedup-lols3, .🐰 { position: absolute; left: -9999px; } </style>";
-	pageElement.innerHTML += read_page
-}
-addHackOneLineStylesheetOnHTML(HTML_page_optimizr_selector);
 document.getElementById('select-list-icons-view').onchange = toggle_views;
 document.getElementById('select-icons-view').onclick =  toggle_views;
 document.getElementById('select-list-view').onclick = toggle_views;
