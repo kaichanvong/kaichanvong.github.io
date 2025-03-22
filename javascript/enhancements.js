c2cf97a98114c6f58ef1a94a803270bf053faac2
@kaichanvong <kaivong@hey.com>
2025-03-22 19:09:04 +0000

    edit enhancements.js

--- javascript/enhancements.js
+++ javascript/enhancements.js
@@ -1,55 +1,59 @@
 var socialMediaChannelsToggleLink1 = document.getElementById('select-icons-view1'),
-	socialMediaChannelsToggleLink2 = document.getElementById('select-list-view2'),
-	socialMediaDiv = document.getElementById('SMChannelsCompleteContent'),
-	linkedContent = document.getElementById('linkedContent'),
-	socialMediaChannels = { 1: 'list' , 0: 'icons'},
-	huh = "<li class=\"bi horizontal-placeholder\"><em>❓</em></li>",
-	instagram_huh = "<li class=\"bi bi-instagram sm-slots\"><a href=\"http://kaivong.com/4ABV\" id=\"instagramLinkContent\" title=\"instagram of kai\" rel=\"me\"><span><strong>instagram</strong></span></a></li>",
-	more = "<li class=\"bi horizontal-placeholder\"><em>…</em></li>",
+    socialMediaChannelsToggleLink2 = document.getElementById('select-list-view2'),
+    socialMediaDiv = document.getElementById('SMChannelsCompleteContent'),
+    linkedContent = document.getElementById('linkedContent'),
+    socialMediaChannels = { 1: 'list' , 0: 'icons'},
+    huh = "<li class=\"bi horizontal-placeholder\"><em>❓</em></li>",
+    instagram_huh = "<li class=\"bi bi-instagram sm-slots\"><a href=\"http://kaivong.com/4ABV\" id=\"instagramLinkContent\" title=\"instagram of kai\" rel=\"me\"><span><strong>instagram</strong></span></a></li>",
+    more = "<li class=\"bi horizontal-placeholder\"><em>…</em></li>",
 
 select_list_icons_view = '';
-select_list_icons_view += '	<option value="select-icons-view">';
-select_list_icons_view += '		<a href="#digitalIdentityContent" id="select-icons-view1" data-name="view-icons">icons</a>';
-select_list_icons_view += '	</option>';
-select_list_icons_view += '	<option value="select-list-view">';
-select_list_icons_view += '		<a href="#digitalIdentityContent" id="select-list-view2" data-name="view-list">list</a>';
-select_list_icons_view += '	</option>';
+select_list_icons_view += '<option value="select-icons-view">';
+select_list_icons_view += '<a href="#digitalIdentityContent" id="select-icons-view1" data-name="view-icons">icons</a>';
+select_list_icons_view += '</option>';
+select_list_icons_view += '<option value="select-list-view">';
+select_list_icons_view += '<a href="#digitalIdentityContent" id="select-list-view2" data-name="view-list">list</a>';
+select_list_icons_view += '</option>';
 document.getElementById('select-list-icons-view').innerHTML = select_list_icons_view;
 
 HTMLPageContentNode = document.getElementById('HTMLPageContent');
 previous_page_class_attrs = HTMLPageContentNode.getAttribute('class')
-if (previous_page_class_attrs != null){
-	previous_page_class_attrs += ' has-javascript html2025';
+page_is_html1999 = (previous_page_class_attrs != null)
+
+if (page_is_html1999){
+  previous_page_class_attrs += ' has-javascript html2025';
+  instagram_huh += ' <span style="visibility: hidden;">.</span>'
 } else { previous_page_class_attrs = 'has-javascript html2025' }
 HTMLPageContentNode.setAttribute('class', previous_page_class_attrs);
+
 linkedContent.innerHTML = instagram_huh + linkedContent.innerHTML;
 
 kaivong_com_objs = { socialMediaDiv: { views: ['view-icons','view-list'] }  }
 function toggle_views(){
-	if(this.nodeName == undefined || this.nodeName == 'SELECT' ){
-		index_for_view = document.getElementById('select-list-icons-view').selectedIndex,
-		new_view = kaivong_com_objs['socialMediaDiv']['views'][index_for_view];	
-	}
-	if(this.nodeName == 'A'){
-		$button = this.nodeName;
-		index_for_view = this.attributes['data-social-media-channel-num'].value,
-		new_view = kaivong_com_objs['socialMediaDiv']['views'][index_for_view];	
-	}
-	socialMediaDiv.setAttribute('class', new_view);
-	socialMediaDiv.setAttribute('data-view', new_view);
-	localStorage.setItem('kaivong_com_socialmedia_view', new_view);
+    if(this.nodeName == undefined || this.nodeName == 'SELECT' ){
+        index_for_view = document.getElementById('select-list-icons-view').selectedIndex,
+        new_view = kaivong_com_objs['socialMediaDiv']['views'][index_for_view];    
+    }
+    if(this.nodeName == 'A'){
+        $button = this.nodeName;
+        index_for_view = this.attributes['data-social-media-channel-num'].value,
+        new_view = kaivong_com_objs['socialMediaDiv']['views'][index_for_view];    
+    }
+    socialMediaDiv.setAttribute('class', new_view);
+    socialMediaDiv.setAttribute('data-view', new_view);
+    localStorage.setItem('kaivong_com_socialmedia_view', new_view);
 };
 
 if(localStorage.key('kaivong_com_socialmedia_view')){
-	viewName = localStorage['kaivong_com_socialmedia_view'],
-	viewIndex = kaivong_com_objs['socialMediaDiv']['views'].indexOf(viewName);
-	document.getElementById('select-list-icons-view').selectedIndex = viewIndex;
-	toggle_views();
+    viewName = localStorage['kaivong_com_socialmedia_view'],
+    viewIndex = kaivong_com_objs['socialMediaDiv']['views'].indexOf(viewName);
+    document.getElementById('select-list-icons-view').selectedIndex = viewIndex;
+    toggle_views();
 }
 
 javascript_enabled_message = document.getElementById('off-page-markedup-lols2');
 javascript_enabled_message.setAttribute('class', 'off-page--HTML-only');
-	
+
 document.getElementById('select-list-icons-view').onchange = toggle_views;
 document.getElementById('select-icons-view').onclick =  toggle_views;
 document.getElementById('select-list-view').onclick = toggle_views;
